a579bec98f414fc186c635a7fcf36888269fe965
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 08:28:52 -0500

    feat : add comments hotel.dart

--- lib/Profiles/hotels/models/hotel.dart
+++ lib/Profiles/hotels/models/hotel.dart
@@ -1,31 +1,36 @@
+// Define the Hotel class to represent a hotel entity
 class Hotel {
-  final int? id;
-  final String? name;
-  final String? address;
-  final int? phoneNumber;
-  final String? description;
-  final String? email;
-  final int? ownerId;
+  
+  // Define the properties of the Hotel class
+  final int? id;  // Optional unique identifier for the hotel
+  final String? name;  // Optional name of the hotel
+  final String? address;  // Optional address of the hotel
+  final int? phoneNumber;  // Optional phone number of the hotel
+  final String? description;  // Optional description of the hotel
+  final String? email;  // Optional email address of the hotel
+  final int? ownerId;  // Optional owner ID of the hotel
 
+  // Constructor to initialize the Hotel object with the required fields
   Hotel({
-    required this.id,
-    required this.name,
-    required this.address,
-    required this.phoneNumber,
-    required this.email,
-    required this.description,
-    required this.ownerId
+    required this.id,  // Required hotel ID
+    required this.name,  // Required name
+    required this.address,  // Required address
+    required this.phoneNumber,  // Required phone number
+    required this.email,  // Required email address
+    required this.description,  // Required description
+    required this.ownerId  // Required owner ID
   });
 
+  // Factory constructor to create an instance of Hotel from a JSON map
   factory Hotel.fromJson(Map<String, dynamic> json) {
     return Hotel(
-      id: json['id'] as int,
-      name: json['name'] as String,
-      address: json['address'] as String,
-      ownerId: json['ownersId'] as int,
-      description: json['description'] as String,
-      phoneNumber: json['phone'] as int,
-      email: json['email'] as String
+      id: json['id'] as int,  // Map hotel ID from JSON
+      name: json['name'] as String,  // Map hotel name from JSON
+      address: json['address'] as String,  // Map hotel address from JSON
+      ownerId: json['ownersId'] as int,  // Map owner ID from JSON
+      description: json['description'] as String,  // Map description from JSON
+      phoneNumber: json['phone'] as int,  // Map phone number from JSON
+      email: json['email'] as String  // Map email from JSON
     );
   }
 }
