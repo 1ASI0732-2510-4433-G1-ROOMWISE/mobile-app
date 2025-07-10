1f579ca2d2a7cedcd03e10d76585d0a2e8c89ba3
Fernando Salgado <129983083+fersalgado26@users.noreply.github.com>
2025-07-10 15:16:12 -0500

    Update role_response.dart

--- lib/Commerce/models/role_response.dart
+++ lib/Commerce/models/role_response.dart
@@ -2,8 +2,8 @@
 class RoleResponse {
   
   // Define the property to store the role value
-  String role;  // The role associated with the response (e.g., 'admin', 'user')
+  String role;  // The role associated with the response ('admin', 'user')
 
-  // Constructor to initialize the RoleResponse object with the required role
+  // Constructor to initialize the RoleResponse object with the required role.
   RoleResponse({required this.role});
 }
